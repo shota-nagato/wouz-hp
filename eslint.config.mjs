a3c3e21f0b28e846729b3f2cc01b24fefb55bb33
shota-nagato <shota.nagato@gmail.com>
2025-10-24 22:14:55 +0900

    Lint, Prettier設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,35 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' },
+      ],
+      'no-console': 'warn',
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    },
+  },
+])
 
-export default eslintConfig;
+export default eslintConfig
