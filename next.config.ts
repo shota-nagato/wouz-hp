090f005b6bf993c51ce19eb08d77376aa273c7c3
shota-nagato <shota.nagato@gmail.com>
2025-10-25 13:57:29 +0900

    feat: deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+}
 
-export default nextConfig;
+export default nextConfig
